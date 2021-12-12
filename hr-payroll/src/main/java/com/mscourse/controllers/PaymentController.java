810e9c2e6fd6b489a832ee7eb5bf1c5db8787915
Jonathan Felix <jhow.wi56@gmail.com>
2021-12-12 13:26:50 -0300

    restTemplate

--- hr-payroll/src/main/java/com/mscourse/controllers/PaymentController.java
+++ hr-payroll/src/main/java/com/mscourse/controllers/PaymentController.java
@@ -1,5 +1,7 @@
 package com.mscourse.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mscourse.entities.Payment;
+import com.mscourse.entities.Worker;
 import com.mscourse.services.PaymentService;
 
 @RestController
@@ -16,12 +19,12 @@ public class PaymentController {
 	
 	@Autowired
 	public PaymentService service;
-	
-	
+		
 	@GetMapping(value="/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
+	
 
 }
--- hr-payroll/src/main/java/com/mscourse/services/PaymentService.java
+++ hr-payroll/src/main/java/com/mscourse/services/PaymentService.java
@@ -1,15 +1,33 @@
 package com.mscourse.services;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.mscourse.entities.Payment;
+import com.mscourse.entities.Worker;
 
 @Service
 public class PaymentService {
-	
-	public Payment getPayment(Long woerkerId, int days) {
-		return new Payment("Bob", 200.0, days);
-		
+
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	public Payment getPayment(Long workerId, int days) {
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", "" + workerId);
+
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+
 	}
 
 }
