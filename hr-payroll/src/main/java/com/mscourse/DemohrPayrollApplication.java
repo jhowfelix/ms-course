7e5ae6c2278fb8615b930850b53a9ec6a6558918
Jonathan Felix <jhow.wi56@gmail.com>
2021-12-12 12:02:27 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/main/java/com/mscourse/DemohrPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.mscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemohrPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemohrPayrollApplication.class, args);
+	}
+
+}
---
+++ hr-payroll/src/test/java/com/mscourse/DemohrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemohrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
