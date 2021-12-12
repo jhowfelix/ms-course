fb8b794a13c19ffcdad5be99fe552e330767d48a
Jonathan Felix <jhow.wi56@gmail.com>
2021-12-12 11:57:54 -0300

    add h2, entity worker, findAll&findById

---
+++ hr-worker/src/main/java/com/ms/controllers/WorkerController.java
@@ -0,0 +1,33 @@
+package com.ms.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ms.entities.Worker;
+import com.ms.repositories.WorkerRepository;
+
+@RestController
+@RequestMapping(value="/workers")
+public class WorkerController {
+	
+	@Autowired
+	private WorkerRepository repo;
+
+	@GetMapping
+	public ResponseEntity<List<Worker>> findAll(){
+		List<Worker> list = repo.findAll();
+		return ResponseEntity.ok(list);
+	}
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable("id") Long id){
+		Worker obj = repo.findById(id).get();
+		return ResponseEntity.ok(obj);
+	}
+	
+}
---
+++ hr-worker/src/main/java/com/ms/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.ms.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ms.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>{
+
+}
