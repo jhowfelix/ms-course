810da28f13f415cea3da3e4604e65d2e8652508b
Jonathan Felix <jhow.wi56@gmail.com>
2021-12-15 16:55:24 -0300

    Private Git repository

--- hr-worker/src/main/java/com/ms/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/ms/controllers/WorkerController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,16 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ms.entities.Worker;
 import com.ms.repositories.WorkerRepository;
 
-
+@RefreshScope
 @RestController
 @RequestMapping(value="/workers")
 public class WorkerController {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
-	
+
 	@Autowired
 	private Environment env;
 	
@@ -34,7 +33,6 @@ public class WorkerController {
 	
 	@GetMapping(value="/configs")
 	public ResponseEntity<Void> getConfigs(){
-	logger.info("CONFIG = " +testConfig);
 	return ResponseEntity.noContent().build();
 	}
 
