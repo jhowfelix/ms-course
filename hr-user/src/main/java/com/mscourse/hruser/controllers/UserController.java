e2bb2bf8a5d3cd9713e13d937b046313666815c4
Jonathan Felix <jhow.wi56@gmail.com>
2021-12-18 13:57:38 -0300

    hr-user repository

--- hr-user/src/main/java/com/mscourse/hruser/controllers/UserController.java
+++ hr-user/src/main/java/com/mscourse/hruser/controllers/UserController.java
@@ -1,21 +1,41 @@
 package com.mscourse.hruser.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mscourse.hruser.entities.User;
+import com.mscourse.hruser.repositories.UserRepository;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserController {
 	
-	User user = new User(1l, "jhow", "jasjdw", "jahsdw");
 	
+	@Autowired
+	private UserRepository repo;
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		return ResponseEntity.ok(repo.findById(id).get());
+	}
 	
 	@GetMapping
-	public User teste() {
-		return user;
+	public List<User> findAll() {
+		return repo.findAll(); 
 	}
+	
+	@GetMapping(value = "/search/{email}")
+	public ResponseEntity<User> findByEmail(@PathVariable String email) {
+		return ResponseEntity.ok(repo.findByEmail(email));
+	}
+	
+	
 
 }
