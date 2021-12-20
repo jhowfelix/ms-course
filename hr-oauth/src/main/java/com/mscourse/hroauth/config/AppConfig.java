6bfc5ef1029d04fe7c0a9cb3f12e9cc1fc7f675f
Jonathan Felix <jhow.wi56@gmail.com>
2021-12-20 17:29:02 -0300

    Login, jwt token

--- hr-oauth/src/main/java/com/mscourse/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/mscourse/hroauth/config/AppConfig.java
@@ -1,41 +1,38 @@
 package com.mscourse.hroauth.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@EnableWebSecurity
-
-public class AppConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-
-	public void configure(HttpSecurity http) throws Exception {
-
-	// @formatter:off
-
-    http
-
-        .csrf()
-
-        .disable()
-
-        .exceptionHandling()
-
-           
-
-    .and()
-
-        .authorizeRequests()
-
-        .antMatchers("/hr-oauth/**").permitAll();
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 
+@Configuration
+public class AppConfig {
 
-    // @formatter:on
-
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-SECRET-KEY");
+		return tokenConverter;
+	}
+	
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+	
+	
+	
+	
 
 }
--- hr-oauth/src/main/java/com/mscourse/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/mscourse/hroauth/services/UserService.java
@@ -3,6 +3,9 @@ package com.mscourse.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.mscourse.hroauth.entities.User;
@@ -10,19 +13,28 @@ import com.mscourse.hroauth.feignclients.UserFeignClient;
 
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
 	@Autowired
 	private UserFeignClient userFeignClient;
 	
-	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	
 	public User findByEmail(String email) {
 		User user = userFeignClient.findByEmail(email).getBody();
 		if(user == null) {
 			throw new IllegalArgumentException("Email not found");}
-		logger.info("email found" + email);		
+		logger.info("email found " + email);		
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if(user == null) {
+			throw new UsernameNotFoundException("Username not found");}
+		logger.info("email found " + username);		
 		return user;
 	}
 	
