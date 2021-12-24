5e34cfdf1440f417c588a280185494531eb5d252
Jonathan Felix <jhow.wi56@gmail.com>
2021-12-24 20:10:26 -0300

    zuul authorization

--- hr-api-gateway-zuul/src/main/java/com/mscourse/hrapigatewayzull/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/mscourse/hrapigatewayzull/config/AppConfig.java
@@ -1,19 +1,14 @@
-package com.mscourse.hroauth.config;
+package com.mscourse.hrapigatewayzull.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+
 @Configuration
 public class AppConfig {
-
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
+	
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
