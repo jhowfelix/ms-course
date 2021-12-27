978834b5fe74abcf58b5d35f4653e768659cc989
Jonathan Felix <jhow.wi56@gmail.com>
2021-12-27 14:48:40 -0300

    hr-user container

--- hr-user/src/main/java/com/mscourse/hruser/repositories/userRepository.java
+++ hr-user/src/main/java/com/mscourse/hruser/repositories/userRepository.java
@@ -1,12 +1,11 @@
 package com.mscourse.hruser.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.mscourse.hruser.entities.User;
 
 
-@EnableJpaRepositories
+
 public interface userRepository extends JpaRepository<User, Long>{
 	
 	User findByEmail(String email);
